714cb657f69f7ab82a4102a498b449980975da6d
FebbyLimasSendelvant <63177115+FebbyLimasSendelvant@users.noreply.github.com>
2022-07-21 22:44:42 +0800

    javascript file

---
+++ tugasjava.js
@@ -0,0 +1,35 @@
+//soal nomor 1
+//let detik = 3600;
+
+//let ubahJam=convertJam(2);
+
+  //  function convertJam(jam){
+//return jam * detik;
+  //  }
+//console.log(ubahJam);
+
+// soal nomor 2
+//let num1 = 90;
+//let num2 = 90;
+//if (num1 > num2) {
+  //console.log(num1 + " yang besar");
+//} else if (num1 === num2) {
+  //console.log(num1 + " nialainya sama");
+//} else {
+  //console.log(num2 + " yang besar");
+//}
+
+//soal nomor 3
+
+//let jumlahTreveler = 2;
+//let hargaTiket = 600.0;
+//let totalHarga;
+//totalHarga = jumlahTreveler * hargaTiket;
+//console.log(totalHarga);
+
+//soal nomor 4
+
+//let nilai = 100;
+//for (let i = 0; i <= nilai; i++) {
+  //console.log(i);
+//}
